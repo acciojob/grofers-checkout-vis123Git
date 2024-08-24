dcd94730d77e22c8f9a1237bdf7be1df24e84cf3
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2024-08-24 22:16:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,20 +4,17 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 	let total = 0;
-	const tb = document.getElementsByTagName("table")[0];
-	console.log("tb==",tb)
-	const allItems = document.querySelectorAll(".price")
-	console.log("allItems==",allItems)
-	const lastRow = document.createElement("tr");
-	const lastTd = document.createElement("td");
-	lastRow.appendChild(lastTd);
-	tb.appendChild(lastRow);
-	for (let i = 0; i < allItems.length; i++) {
-	 console.log("allItems[i]=="allItems[i])
-		
-	}
-	// allItems.forEach((el)=>{
-	// })
+	const allItems = Array.from(document.querySelectorAll(".price"))
+    allItems.forEach((element) => {
+        total += parseFloat(element.textContent);
+	})
+	    const table = document.querySelector("table");
+    const lastRow = document.createElement("tr");
+    const lastCell = document.createElement("td");
+    lastCell.colSpan = 2;
+    lastCell.textContent = total;
+    lastRow.appendChild(lastCell);
+    table.appendChild(lastRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
